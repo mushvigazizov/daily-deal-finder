15afa32adda4b606117d0f581cbd582fbeaa7efc
Mushvig Azizov <mushvig.azizov@gmail.com>
2026-07-11 06:34:13 +0000

    Add multilingual product content loader

---
+++ core/i18n/__init__.py
@@ -0,0 +1,1 @@
+# Daily Deal Finder i18n package
---
+++ core/i18n/config.py
@@ -0,0 +1,10 @@
+SUPPORTED_LANGUAGES = [
+    "de",
+    "en",
+    "ru",
+]
+
+DEFAULT_LANGUAGE = "en"
+
+CONTENT_DIRECTORY = "data/content"
+TRANSLATION_DIRECTORY = "data/i18n"
